12d1ed86e637d9fb30922146274077b3c7e841d7
msdemirel <aa2996@student.jamk.fi>
2022-02-06 20:20:53 +0200

    Added particle system

--- Game of Thrones Jam/Assets/CollectGems.cs
+++ Game of Thrones Jam/Assets/CollectGems.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class CollectGems : MonoBehaviour
 {
+    public GameObject collectEffect;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         this.gameObject.SetActive(false);
 
+        Instantiate(collectEffect, transform.position, Quaternion.identity);
 
     }
+
+    
 }
